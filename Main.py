9c00723ac005231df2d551ae88c1b29e377cd5d5
Lime <64971922+Lime667@users.noreply.github.com>
2020-11-15 19:40:51 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
-
-template = """
-
+import os
+banner = '''
 ███╗   ███╗ ██████╗ ██████╗ ██╗   ██╗██╗      █████╗ ██████╗ 
 ████╗ ████║██╔═══██╗██╔══██╗██║   ██║██║     ██╔══██╗██╔══██╗
 ██╔████╔██║██║   ██║██║  ██║██║   ██║██║     ███████║██████╔╝
@@ -19,33 +18,60 @@ This little script help you to find some congruences and more generally to work 
 This can be helpful when you're working with very large integer.
 Was made in order to learn some basics mathematicals principes and help me
 to do some calc in a ring.
-
-MODE : 
-[1] Find somes congruences between 2 numbers on a given interval. 
-[2] Check if two numbers are congruent modulo n. (for a,b,n given)
-[3] Check if a number is invertible in a given ring.
+'''
+template = """
+MODE :                                                              
+[1] Find somes congruences between 2 numbers on a given interval.              |  [b] Print the banner
+[2] Check if two numbers are congruent modulo n. (for a,b,n given)             |  [e] Exit
+[3] Check if a number is invertible in a given ring.                           |
 [] Developpement
 
 Selected mode : """
 
-
-
-def exp_res(c):
-    export = input("Export results in txt file ? y/n ")
-    if export == "y" or export == "Y":
-        name = input("Path to file(including name file, empty is current folder): ")
-        if name == "":
-            name = "CongruenceExport.txt"
+class utils:
+    def exp_res(c):
+        export = input("Export results in txt file ? y/n ")
+        if export == "y" or export == "Y":
+            name = input("Path to file(including name file, empty is current folder): ")
+            if name == "":
+                name = "CongruenceExport.txt"
+            else:
+                name = name
+            file = open(name, "w",encoding="utf-8")
+            var = ""
+            for i in c:
+                var += i+"\n"
+            file.write(var)
+            file.close()
         else:
-            name = name
-        file = open(name, "w",encoding="utf-8")
-        var = ""
-        for i in c:
-            var += i+"\n"
-        file.write(var)
-        file.close()
-    else:
-        pass
+            pass
+    
+    def backtomenu():
+        m = "value"
+        while m!="":
+            m = input("Press enter to go back to the menu...")
+        main_menu()
+
+    def pgcd(a,b):
+        if a > b:
+            if b==0:
+                return a
+            else:
+                r=a%b
+                return(utils.pgcd(b,r))
+        if b > a:
+            if a ==0:
+                return b
+            else:
+                r=b%a
+                return(utils.pgcd(a,r))
+
+    def bezout_coef(a, b):
+        if b == 0:
+            return (1, 0)
+        else:
+            (u, v) = utils.bezout_coef(b, a % b)
+            return (v, u - (a // b) * v)
 
 def congru(a,b):
     pol = input("Enter two numbers to set the interval (empty is 2-100) : ")
@@ -57,7 +83,7 @@ def congru(a,b):
             if a%x == b%x:
                 c.append(str(a)+" ≡ "+str(b)+" modulo "+str(x))
                 print(a,"≡",b,"modulo",x)
-        exp_res(c)
+        utils.exp_res(c)
     else:
         pal = pol.split("-")
         print(pal)
@@ -67,7 +93,9 @@ def congru(a,b):
             if a%x == b%x:
                 c.append(str(a)+" ≡ "+str(b)+" modulo "+str(x))
                 print(a,"≡",b,"modulo",x)
-        exp_res(c)
+        utils.exp_res(c)
+        utils.backtomenu()
+
 
 def is_congru(a,b,n):
     if (a-b)%n == 0:
@@ -75,33 +103,25 @@ def is_congru(a,b,n):
         print(a,"≡",b,"["+n+"]")
     else:
         print("[*]",a,"is not congruent to",b,"modulo",n)
+    utils.backtomenu()
 
-def pgcd(a,b):
-    if a > b:
-        if b==0:
-            return a
-        else:
-            r=a%b
-            return(pgcd(b,r))
-    if b > a:
-        if a ==0:
-            return b
-        else:
-            r=b%a
-            return(pgcd(a,r))
 
 def inverse(a,n):
-    if pgcd(a,n) ==1:
+    if utils.pgcd(a,n) ==1:
         print(a,"is invertible in Z/"+str(n)+"Z")
         print("To doing that, we just calc the pgcd of",a,"and",n,". If they're relatively prime,",a,"is invertible.")
-        print("Proof by the bachet-bezout theorem.")
+        print("We can prove it by the bachet-bezout theorem.")
+        print("Let's find the invert. We need to calcul the bezout relation between",a,"and",n,"\n")
+        b,c = utils.bezout_coef(a,n)
+        print(b%n,"is the invert of",a)
+        print("The invert of",-b,"is",-a%n)
     else:
         print(a,"is not invertible in Z/"+str(n)+"Z")
-
+    utils.backtomenu()
 
 def main_menu():
     UIN = ""
-    while UIN != "1" and UIN != "2" and UIN != "3":
+    while UIN != "1" and UIN != "2" and UIN != "3" and UIN != "b" and UIN != "e":
         UIN = input(template)
     if UIN == "1":
         a,b = int(input("a : ")),int(input("b : "))
@@ -112,5 +132,13 @@ def main_menu():
     elif UIN == "3":
         a,n = int(input("number to check : ")), int(input("Ring value : "))
         inverse(a,n)
+    elif UIN == "b":
+        print(banner)
+        backtomenu()
+    elif UIN == 'e' or UIN == "E":
+        exit("Thanks.")
 
-main_menu()
+if __name__ == "__main__":
+    os.system('cls' if os.name =='nt' else "clear")
+    print(banner)
+    main_menu()
